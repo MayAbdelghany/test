614074ebbbe692a3b4d80890a39f43f08da8a681
may abdelghany awaad <115990437+MayAbdelghany@users.noreply.github.com>
2023-04-28 21:25:19 +0200

    new

--- index.js
+++ index.js
@@ -1,45 +1,82 @@
+
+
 import express from 'express'
 import dotenv from 'dotenv'
 import cors from 'cors'
+import pdf from "pdf-creator-node";
+import fs from "fs";
 
-import { createInvoice } from "./pdf.js";
+import cloudinary from './cloudinary.js';
+import sendEmail from './cloudinary.js';
 
 const app = express()
 dotenv.config()
 app.use(cors())
+app.use(express.json())
 const port = process.env.port
 
-const invoice = {
-    shipping: {
-        name: "John Doe",
-        address: "1234 Main Street",
-        city: "San Francisco",
-        state: "CA",
-        country: "US",
-        postal_code: 94111
-    },
-    items: [
+
+
+app.get('/', async (req, res) => {
+    // Read HTML Template
+    var html = fs.readFileSync("template.html", "utf8");
+    const bitmap = fs.readFileSync("./logo.png")
+    const logo = bitmap.toString('base64');
+    var options = {
+        format: "A3",
+        orientation: "portrait",
+        border: "10mm",
+        header: {
+            height: "45mm",
+            contents: '<div style="text-align: center;">Author: Shyam Hajare</div>'
+        },
+        footer: {
+            height: "28mm",
+            contents: {
+                first: 'Cover page',
+                2: 'Second page', // Any page number is working. 1-based index
+                default: '<span style="color: #444;">{{page}}</span>/<span>{{pages}}</span>', // fallback value
+                last: 'Last Page'
+            }
+        }
+    };
+    var users = [
         {
-            item: "TC 100",
-            description: "Toner Cartridge",
-            quantity: 2,
-            amount: 6000
+            name: "Shyam",
+            age: "26",
+        
         },
         {
-            item: "USB_EXT",
-            description: "USB Cable Extender",
-            quantity: 1,
-            amount: 2000
-        }
-    ],
-    subtotal: 8000,
-    paid: 0,
-    invoice_nr: 1234
-};
-app.get('/', (req, res, next) => {
-    createInvoice(invoice, "invoice.pdf");
-    res.json({ message: "hi" })
-})
+            name: "Navjot",
+            age: "26",
+          
+        },
+        {
+            name: "Vitthal",
+            age: "26",
+           
+        },
+    ];
+    var document = {
+        html: html,
+        data: {
+            users: users,
+            logo: logo
+        },
+        path: "./output.pdf",
+        type: "",
+    };
+    pdf
+        .create(document, options)
+        .then(async (res) => {
+            const { secure_url, public_id } = await cloudinary.uploader.upload("./output.pdf")
+            console.log({ secure_url, public_id });
+        })
+        .catch((error) => {
+            console.error(error);
+        });
+    res.json({ message: "done" })
 
+})
 
 app.listen(port, () => console.log(`runing in port 3000`))
