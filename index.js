53a565d6dc7aafbb40b501e50f9d33b9ade9c776
may abdelghany awaad <115990437+MayAbdelghany@users.noreply.github.com>
2023-04-28 22:09:44 +0200

    a

--- index.js
+++ index.js
@@ -22,65 +22,70 @@ app.get('/', async (req, res) => {
 })
 
 app.get('/hi', async (req, res) => {
-    let newPath = path.join(__dirname, './template.html')
+    try {
+        let newPath = path.join(__dirname, './template.html')
 
-    var html = fs.readFileSync(newPath, "utf8");
-    let newPath2 = path.join(__dirname, './logo.png')
-    const bitmap = fs.readFileSync(newPath2)
-    const logo = bitmap.toString('base64');
-    var options = {
-        format: "A3",
-        orientation: "portrait",
-        border: "10mm",
-        header: {
-            height: "45mm",
-            contents: '<div style="text-align: center;">Author: Shyam Hajare</div>'
-        },
-        footer: {
-            height: "28mm",
-            contents: {
-                first: 'Cover page',
-                2: 'Second page', // Any page number is working. 1-based index
-                default: '<span style="color: #444;">{{page}}</span>/<span>{{pages}}</span>', // fallback value
-                last: 'Last Page'
+        var html = fs.readFileSync(newPath, "utf8");
+        let newPath2 = path.join(__dirname, './logo.png')
+        const bitmap = fs.readFileSync(newPath2)
+        const logo = bitmap.toString('base64');
+        var options = {
+            format: "A3",
+            orientation: "portrait",
+            border: "10mm",
+            header: {
+                height: "45mm",
+                contents: '<div style="text-align: center;">Author: Shyam Hajare</div>'
+            },
+            footer: {
+                height: "28mm",
+                contents: {
+                    first: 'Cover page',
+                    2: 'Second page', // Any page number is working. 1-based index
+                    default: '<span style="color: #444;">{{page}}</span>/<span>{{pages}}</span>', // fallback value
+                    last: 'Last Page'
+                }
             }
-        }
-    };
-    var users = [
-        {
-            name: "Shyam",
-            age: "26",
+        };
+        var users = [
+            {
+                name: "Shyam",
+                age: "26",
 
-        },
-        {
-            name: "Navjot",
-            age: "26",
+            },
+            {
+                name: "Navjot",
+                age: "26",
 
-        },
-        {
-            name: "Vitthal",
-            age: "26",
+            },
+            {
+                name: "Vitthal",
+                age: "26",
 
-        },
-    ];
-    var document = {
-        html: html,
-        data: {
-            users: users,
-            logo: logo
-        },
-        path: "./output.pdf",
-        type: "",
-    };
-    await pdf
-        .create(document, options)
-    res.json({ message: "aaaaaaaaaaaaaaa" })
-    // let newPath3 = path.join(__dirname, './output.pdf')
-    // const { secure_url, public_id } = await cloudinary.uploader.upload(newPath3)
+            },
+        ];
+        let newPath3 = path.join(__dirname, './output.pdf')
+        var document = {
+            html: html,
+            data: {
+                users: users,
+                logo: logo
+            },
+            path: newPath3,
+            type: "",
+        };
+        await pdf
+            .create(document, options)
+        res.json({ message: "aaaaaaaaaaaaaaa" })
+        // let newPath3 = path.join(__dirname, './output.pdf')
+        // const { secure_url, public_id } = await cloudinary.uploader.upload(newPath3)
 
 
-    // res.json({ message: "done", "details": { secure_url, public_id } })
+        // res.json({ message: "done", "details": { secure_url, public_id } })
 
+    } catch (error) {
+        res.json({ message: "error" })
+    }
 })
 
 app.listen(port, () => console.log(`runing in port 3000`))
